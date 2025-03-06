93da3f4901d632240801f19fdbf69a4b5bee9112
Tho Ho <code@tho.email>
2025-03-06 10:37:56 +0800

    Clear using Windows Event Log

--- GarnetHost/Program.cs
+++ GarnetHost/Program.cs
@@ -1,6 +1,9 @@
 using NetLah.GarnetHost;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 builder.Services.AddHostedService(_ => new Worker(args));
 
 builder.Services.AddWindowsService(options =>
